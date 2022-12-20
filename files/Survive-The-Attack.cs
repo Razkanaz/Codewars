c628dc5c8e3b53b0c18e080dbb2bd2aeb7da5878
Razkanaz <azneeisdumb@gmail.com>
2022-12-20 21:59:15 +0700

    Update 2
    - implemented basic elements

--- files/Survive-The-Attack.cs
+++ files/Survive-The-Attack.cs
@@ -5,14 +5,41 @@ namespace Program
     internal class Program
     {
 
-        public static bool HasSurvived(int[] attackers, int[] defenders)
-        { 
-            return true;
+        public static string[] HasSurvived(int[] attackers, int[] defenders)
+        {
+            bool WhichIsLonger = attackers.Length <= defenders.Length;
+            int n = WhichIsLonger ? defenders.Length:attackers.Length;
+            string[] temp = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    if (attackers[i] == defenders[i])
+                    {
+                        temp[i] = "empty"; 
+                        continue;
+                    }
+
+                    temp[i] = attackers[i] > defenders[i] ? "Attackers":"Defenders";
+                }
+                catch (System.Exception)
+                {
+                    
+                }
+            }
+
+            return temp;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("wow");
+            string[] h = HasSurvived(new[] {3,2,1}, new[] {1,2,3});
+
+            foreach (string item in h)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
