3ec628cee0e8e66e62afe60aeabf049c4fcbacec
Razkanaz <azneeisdumb@gmail.com>
2022-12-20 20:00:24 +0700

    finally fixing the bug

---
+++ STA.cs
@@ -0,0 +1,17 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Internal;
+
+namespace TSA
+{
+    public class STA
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("hello, world");
+        }
+    }
+}
