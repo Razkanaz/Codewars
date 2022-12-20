6a122676a24ac9e6b60980d11ef1ff9b5545dee2
Razkanaz <azneeisdumb@gmail.com>
2022-12-20 21:19:33 +0700

    Update 1
    - added the main function that's gonna do the work

--- files/Survive-The-Attack.cs
+++ files/Survive-The-Attack.cs
@@ -4,6 +4,12 @@ namespace Program
 {
     internal class Program
     {
+
+        public static bool HasSurvived(int[] attackers, int[] defenders)
+        { 
+            return true;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("wow");
