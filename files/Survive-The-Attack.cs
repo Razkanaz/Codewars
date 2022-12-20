d7f5061b4396f31eddc0ebbf55773ff7229ed158
Razkanaz <azneeisdumb@gmail.com>
2022-12-20 23:29:29 +0700

    Final Update
    -fixed a bug where the output was the opposite of the desired effect
    -finished the rest

--- files/Survive-The-Attack.cs
+++ files/Survive-The-Attack.cs
@@ -5,41 +5,52 @@ namespace Program
     internal class Program
     {
 
-        public static string[] HasSurvived(int[] attackers, int[] defenders)
+        public static bool HasSurvived(int[] attackers, int[] defenders)
         {
-            bool WhichIsLonger = attackers.Length <= defenders.Length;
-            int n = WhichIsLonger ? defenders.Length:attackers.Length;
-            string[] temp = new string[n];
 
+            //variables
+            //checks which array is longer
+            bool WhichIsLonger = attackers.Length <= defenders.Length;
+            //gets the length of the longest array
+            int n = WhichIsLonger ? defenders.Length:attackers.Length;
+            //sets the array string
+            int[] temp = new int[3];
+
+            //main loop for step 1
             for (int i = 0; i < n; i++)
             {
                 try
                 {
                     if (attackers[i] == defenders[i])
                     {
-                        temp[i] = "empty"; 
                         continue;
                     }
 
-                    temp[i] = attackers[i] > defenders[i] ? "Attackers":"Defenders";
+                    temp[attackers[i] > defenders[i] ? 1:2] += 1 ;
                 }
                 catch (System.Exception)
                 {
-                    temp[i] = WhichIsLonger ? "Defenders":"Attackers";
+                    temp[WhichIsLonger ? 2:1] += 1;
                 }
             }
 
-            return temp;
+            int Asum = 0, Dsum = 0;
+            Array.ForEach(attackers, delegate(int i) {Asum += i; });
+            Array.ForEach(defenders, delegate(int i) {Dsum += i; });
+
+            if (temp[2] == temp[1])
+            {
+                return Asum <= Dsum; 
+            }
+
+            return temp[1] < temp[2];
         }
 
         static void Main(string[] args)
         {
-            string[] h = HasSurvived(new[] {3,2,1}, new[] {1,2,3,0});
+            bool h = HasSurvived(new[] {2, 9, 9, 7}, new[] {1, 1, 3, 8});
 
-            foreach (string item in h)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(h ? "The battle is WON!!!":"the battle is sadly lost");
         }
     }
 }
