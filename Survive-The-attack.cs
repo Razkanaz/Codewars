fac6fa5001041f638453e57c6d92fd356598c7c2
Razkanaz <azneeisdumb@gmail.com>
2022-12-20 08:39:34 +0700

    Create Survive-The-attack.cs

---
+++ Survive-The-attack.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Kata
+{
+
+    static void main (string[] args)
+    {
+        Console.WriteLine("test");
+    }
+
+    public static bool HasSurvived(int[] attackers, int[] defenders)
+    {
+        foreach (int i in attackers)
+        {
+            
+        }
+    }
+}
